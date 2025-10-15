628b0d469e607a3649ec2e385d7897d0f524f73c
DomanskiFilip <filip_domanski@outlook.com>
2025-10-15 10:30:02 +0100

    nav, footer and colour scheme

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
-
 <template>
-  
+    <main>
+        <Nav />
+        <footer>&copy; Filip Domanski <span id="year">{{ year }}</span></footer>
+    </main>
 </template>
 
 <script setup>
-
+    import { ref } from 'vue';
+    import Nav from './components/Nav.vue';
+    
+    const year = new Date().getFullYear();
 </script>
 
 
 <style scoped>
+main {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    background-color: var(--primary);
+}
 
+footer {
+    width: 100%;
+    height: 2rem;
+    background-color: var(--secondary);
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    gap: 0.25rem;
+}
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,90 @@
+<template>
+    <section>
+        <h1>ClassShop</h1>
+        <div class="center">
+            <input type="text" id="search" placeholder="Search..." />
+            <button id="taglist"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m159-168-34-14q-31-13-41.5-45t3.5-63l72-156v278Zm160 88q-33 0-56.5-23.5T239-160v-240l106 294q3 7 6 13.5t8 12.5h-40Zm206-4q-32 12-62-3t-42-47L243-622q-12-32 2-62.5t46-41.5l302-110q32-12 62 3t42 47l178 488q12 32-2 62.5T827-194L525-84Zm-86-476q17 0 28.5-11.5T479-600q0-17-11.5-28.5T439-640q-17 0-28.5 11.5T399-600q0 17 11.5 28.5T439-560Zm58 400 302-110-178-490-302 110 178 490ZM319-650l302-110-302 110Z"/></svg></button>
+            <button id="sort"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M120-240v-80h240v80H120Zm0-200v-80h480v80H120Zm0-200v-80h720v80H120Z"/></svg></button>
+        </div>
+        <button id="cart"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z"/></svg></button>
+    </section>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+section {
+    width: 99%;
+    height: 3rem;
+    background-color: var(--secondary);
+    margin: 1rem 0.5em;
+    border-radius: 0.5rem;
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+}#cart:active {
+    background-color: var(--action-active);
+}
+
+.center {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 0.5rem;
+    padding-right: 4.5rem;
+}
+
+h1 {
+    color: white;
+    font-size: 2rem;
+    font-weight: bold;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    position: absolute;
+    left: 1rem; 
+    margin: 0;
+    text-shadow: var(--accent);
+}
+
+#search {
+    height: 2rem;
+    width: 25%;
+    border-radius: 0.5rem;
+    border: none;
+    padding-left: 0.5rem;
+    font-size: 1rem;
+}
+
+#search:focus {
+    outline: 5px solid var(--action);
+}
+
+button {
+    width: 2.5rem;
+    height: 2.5rem;
+    border-radius: 0.5rem;
+    background-color: var(--action);
+    color: var(--text);
+    border: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+button:hover {
+    cursor: pointer;
+    background-color: var(--action-hover);
+}
+
+button:active {
+    background-color: var(--action-active);
+}
+
+#cart {
+    position: absolute;
+    right: 1rem;
+}
+</style>
